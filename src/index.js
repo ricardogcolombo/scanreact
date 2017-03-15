4e4273d1a1e5411ad0b38b5b08f50df2d712828f
ricardogcolombo <ricardogcolombo@gmail.com>
2017-03-15 10:03:28 -0300

    [ADD]Redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import Store from './store';
+
 ReactDOM.render(
-  <App />,
+ <Provider store={StoreInstance}>
+   <App />
+ </Provider>,
   document.getElementById('root')
 );
