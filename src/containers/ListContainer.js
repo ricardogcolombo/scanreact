2f7b0c6847680e8addb57889c1d913b306940ab6
ricardogcolombo <ricardogcolombo@gmail.com>
2017-03-20 20:29:07 -0300

    [ADD]ListItemContainer

---
+++ src/containers/ListContainer.js
@@ -0,0 +1,36 @@
+import React, {
+    Component
+} from 'react';
+
+import {
+    connect
+} from 'react-redux';
+
+import List from "../components/List";
+import store from '../store';
+
+
+class ListContainer extends Component {
+    onItemClick(id) {
+        this.props.onItemSelected(id)
+    }
+
+    render() {
+        let items = (this.props.config.productList.results) ? this.props.config.productList.results : [];
+
+        return (
+            <List items={items} onItemClick={this.onItemClick.bind(this)}/>
+        );
+    }
+}
+
+const mapStateToProps = function(state) {
+    return {
+        config: {
+            product: state.product,
+            productList: state.productList
+        }
+    };
+}
+
+export default connect(mapStateToProps)(ListContainer);
