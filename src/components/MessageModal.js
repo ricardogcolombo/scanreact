d48041a3dc05c4a885f4570f2196b1d3c29dda7a
Alan Campora <alan.campora@gmail.com>
2017-03-21 01:10:22 -0300

    [ADD] Modal content

--- src/components/MessageModal.js
+++ src/components/MessageModal.js
@@ -6,17 +6,18 @@ const ContentModal = ({title, inputTitle, textAreaTitle, submitButtonText, onSub
    return(
     <div className='modal'>
         <div className='modal-form'> 
-            
-            <p className='title'>{title}</p>
-            
-            <p className='title'>{inputTitle}</p>
-            <input className='input'/>
+           <div className='content'>  
+                <p className='title'>{title}</p>
+                
+                <p className='title'>{inputTitle}</p>
+                <input className='input'/>
 
-            <p className='title'>{textAreaTitle}</p>
-            <textarea className='description' rows="4" cols="50" ></textarea>
-    
-            <Button className='button submitButton' onClick={onSubmitPress} text={submitButtonText}/>
-            <Button className='button cancelButton' text='cancelar'/>
+                <p className='title'>{textAreaTitle}</p>
+                <textarea className='description' rows="4" cols="50" ></textarea>
+        
+                <Button className='button submitButton' onClick={onSubmitPress} text={submitButtonText}/>
+                <Button className='button cancelButton' text='cancelar'/>
+            </div> 
        </div>
        
     </div>
