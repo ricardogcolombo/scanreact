a194463c964d4a41de1d6c29d586c5fbefc2ded1
ricardogcolombo <ricardogcolombo@gmail.com>
2017-03-21 19:23:24 -0300

    [ADD]Cordova

---
+++ hexaScanHybrid/platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,20 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-barcodescanner.BarcodeScanner",
+        "file": "plugins/cordova-plugin-barcodescanner/www/barcodescanner.js",
+        "pluginId": "cordova-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-barcodescanner": "0.7.0",
+    "cordova-plugin-compat": "1.1.0",
+    "cordova-plugin-whitelist": "1.3.2"
+};
+// BOTTOM OF METADATA
+});
