bfd67ca47ed765185ac3d1192e53fe65e1272b4c
ricardogcolombo <ricardogcolombo@gmail.com>
2017-03-27 19:46:01 -0300

    [ADD] missing fields into detail view

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Button from '../Button/Button';
 import Field from '../Field/Field';
 
-const Details = ({ product, onSubmitPress, onSendMessage}) => {
+const Details = ({
+    product,
+    onSubmitPress,
+    onSendMessage
+}) => {
 
     // FIXME: remove this and check on detailscontainer the state
     let imgSrc = (product.pic) ? 'data:image/jpeg;base64,' + product.pic : "";
@@ -20,9 +24,26 @@ const Details = ({ product, onSubmitPress, onSendMessage}) => {
             </div>
 
             <div className='Details__descripcion'>
-                <Field label ='Descripcion' value={product.descripcion} />
+                <Field label='Descripcion' value={product.descripcion} />
             </div>
 
+            <div className='dataRow'>
+                <Field label='PS' value={product.PS} />
+                <Field label='PS' value={product.PV} />
+                <Field label='BTOVIG' value={product.BTOVIG} />
+                <Field label='SKTMIN' value={product.SKTMIN} />
+            </div>
+            <div className='dataRow'>
+                <Field label='TIPO' value={product.TIPO} />
+                <Field label='ULTINGCDR' value={product.ULTINGCDR} />
+                <Field label='CAL' value={product.CAL} />
+                <Field label='FORMATO' value={product.FORMATO} />
+            </div>
+
+            <div className='dataRow'>
+                <Field label='ULTVTA' value={product.ULTVTA} />
+                <Field label='FECDISC' value={product.FECDISC} />
+            </div>
             <Button onClick={onSendMessage} text='Enviar mensaje' />
 
         </div>
